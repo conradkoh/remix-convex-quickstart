d73e123f8e37fdc57b378ec0daa7b8f8f1ed8380
conradkoh <conradkoh@gmail.com>
2024-04-16 08:32:36 +0800

    wrap root layout with convex provider

--- app/root.tsx
+++ app/root.tsx
@@ -4,23 +4,44 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  json,
+  useLoaderData,
 } from "@remix-run/react";
-
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { useMemo } from "react";
+export async function loader() {
+  const convexUrl = process.env["CONVEX_URL"];
+  if (!convexUrl) {
+    throw new Error("CONVEX_URL environment variable is required");
+  }
+  return json({
+    ENV: {
+      CONVEX_URL: convexUrl,
+    },
+  });
+}
 export function Layout({ children }: { children: React.ReactNode }) {
+  const loaderData = useLoaderData<typeof loader>();
+  const convex = useMemo(
+    () => new ConvexReactClient(loaderData.ENV.CONVEX_URL),
+    [loaderData.ENV.CONVEX_URL]
+  );
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <ConvexProvider client={convex}>
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
+    </ConvexProvider>
   );
 }
 
